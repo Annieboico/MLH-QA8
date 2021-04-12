b4980840dff030531de8ffb3e15d5f9afcfbbbba
Annieboico <Anastasiia.boico@gmail.com>
2021-04-11 22:56:11 -0700

    added testData

---
+++ data/testData.js
@@ -0,0 +1,20 @@
+const name = {
+    "default": "LadyBug007"
+}
+
+const age = {
+    "default": "1234567890"
+}
+const gender ={
+    "he": 0,
+    "she": 1,
+    "it": 2
+}
+
+const story = {
+    "overcomingTheMonster": 0,
+    "rebirth": 1,
+    "quest": 2,
+    "comedy": 6
+}
+module.exports = {name, age, gender, story};
--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors';
 import exp from '../../data/expected.json';
+import {name, age, gender, story} from '../../data/testData';
 
 describe('Required Fields and story created', function () {
 
@@ -9,11 +10,11 @@ describe('Required Fields and story created', function () {
     })
 
     it('TC-026 Submit button is enabled after all fields filled out with valid values ', function () {
-        $(sel.nameField).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $$(sel.age)[1].setValue('1234567890');
+        $(sel.nameField).setValue(name.default);
+        $$(sel.radioButtons)[gender.he].click();
+        $$(sel.age)[1].setValue(age.default);
         $(sel.storyType).click();
-        $$(sel.chooseStory)[6].click();
+        $$(sel.chooseStory)[story.comedy].click();
         let submit = $(sel.submit).isEnabled();
         expect(submit).toEqual(true);
     });
